d8cbd074853fc418528b57dcb0e1428921205070
Damdev80 <danielbuelvas9624@gmai.com>
2026-03-05 13:46:21 -0500

    fix: update import paths for feature components in body.ts

--- src/app/layout/body/body.ts
+++ src/app/layout/body/body.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { Servicios } from '../../components/servicios/servicios';
-import { Conocenos } from '../../components/conocenos/conocenos';
-import { Equipo } from '../../components/equipo/equipo';
-import { Proyectos } from '../../components/proyectos/proyectos';
-import { Contacto } from '../../components/contacto/contacto';
-import { Faq } from '../../components/faq/faq';
-import { Cta } from '../../components/cta/cta';
+import { Servicios } from '../../feature/servicios/servicios';
+import { Conocenos } from '../../feature/conocenos/conocenos';
+import { Equipo } from '../../feature/equipo/equipo';
+import { Proyectos } from '../../feature/proyectos/proyectos';
+import { Contacto } from '../../feature/contacto/contacto';
+import { Faq } from '../../feature/faq/faq';
+import { Cta } from '../../feature/cta/cta';
 
 @Component({
   selector: 'app-body',
