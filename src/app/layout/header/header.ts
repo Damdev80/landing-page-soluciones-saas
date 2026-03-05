b721742723bf9892910d8c04533551c99a5181b5
Damdev80 <danielbuelvas9624@gmai.com>
2026-03-05 17:26:32 -0500

    feat: mark header component as standalone

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,6 +1,7 @@
 import { Component} from '@angular/core';
 import { Navbar } from '../../feature/navbar/navbar';
 @Component({
+  standalone: true,
   selector: 'app-header',
   imports: [Navbar],
   templateUrl: './header.html',
