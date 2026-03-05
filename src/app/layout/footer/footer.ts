a6d6583a7246623c758160da1e39099274df77ef
Damdev80 <danielbuelvas9624@gmai.com>
2026-03-05 17:23:40 -0500

    feat: implement footer component with structured layout and links

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { FooterComponent } from '../../feature/footer/footer';
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './footer.html',
 })
 export class Footer {}
